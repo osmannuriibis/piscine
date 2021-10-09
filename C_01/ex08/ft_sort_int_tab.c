705b311b86f1887b8279bcabff40fc81ab5e7598
osmannuriibis <osmannuriibis@gmail.com>
2021-10-09 11:58:53 +0300

    adding

---
+++ C_01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oibis <oibis@student.42kocaeli.com.tr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/05 23:31:44 by oibis             #+#    #+#             */
+/*   Updated: 2021/10/06 00:52:50 by oibis            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	counter;
+	int	status;
+	int	holder;
+
+	status = 1;
+	while (status == 1)
+	{	
+		counter = 0;
+		status = 0;
+		while (counter < size - 1)
+		{
+			if (*(tab + counter) > *(tab + 1 + counter))
+			{
+				holder = *(tab + counter);
+				*(tab + counter) = *(tab + counter + 1);
+				*(tab + counter + 1) = holder;
+				status = 1;
+			}
+			counter++;
+		}
+	}
+}
---
+++ C_02/ex12/ft_print_memory.c
@@ -0,0 +1,4 @@
+void	*ft_print_memory(void *adde, unsigned int size)
+{
+
+}
